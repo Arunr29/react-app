54d4c0c19929835ea86551aede1a474931f9fe70
arun@makolet.in <password9nine>
2023-03-27 18:53:06 +0530

    commit props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,21 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
   //Hard-coded variables
-  const expenseDate = new Date(2023, 2, 27);
-  const expenseDescription = "Car Insuarance";
-  const expensePrice = 294.67;
+  // const expenseDate = new Date(2023, 2, 27);
+  // const expenseDescription = "Car Insuarance";
+  // const expensePrice = 294.67;
   const dollar = "$";
-
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}{dollar}</div>
+        <div className="expense-item__price">
+          {props.amount}
+          {dollar}
+        </div>
       </div>
     </div>
   );
